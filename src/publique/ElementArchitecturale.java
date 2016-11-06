49f20c02e89e2ebb69ef0565416fc936ceca5a2e
Hiba <hibabenyahia7@gmail.com>
2016-11-06 09:09:33 +0100

    enregistrement du premier projet HADL M2 (projet complet)

--- src/publique/ElementArchitecturale.java
+++ src/publique/ElementArchitecturale.java
@@ -1,5 +1,22 @@
 package publique;
 
+import connecteur.Connecteur;
+
+import java.util.ArrayList;
+
 public class ElementArchitecturale {
 
+    public String nom;
+
+    public ElementArchitecturale(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 }
